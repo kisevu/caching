5bb52b5fcbba05a4f83715ba212bb8ab92f423f3
ameda-kevin <amedakevin@gmail.com>
2024-02-14 16:30:00 +0300

    typical bootstrap

--- src/main/java/org/ameda/kevin/cachingredis/CachingRedisApplication.java
+++ src/main/java/org/ameda/kevin/cachingredis/CachingRedisApplication.java
@@ -1,12 +1,9 @@
 package org.ameda.kevin.cachingredis;
 
-import org.ameda.kevin.cachingredis.config.SimpleRedisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({SimpleRedisConfig.class})
 public class CachingRedisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CachingRedisApplication.class, args);
